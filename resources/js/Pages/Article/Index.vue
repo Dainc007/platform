627532718e6a50a228eb4b759919c805aabe4f05
Daniel <danielheinze@MacBook-Air-Daniel.local>
2024-07-15 13:35:11 +0200

    Article Vue Components

---
+++ resources/js/Pages/Article/Index.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    articles: Array
+});
+</script>
+
+<template>
+    <Head title="Dashboard" />
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Articles</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div v-for="article in articles" class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg my-2">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">{{article.content}}</div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
