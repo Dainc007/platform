712e7faef7a07c14820b68411810b6a4c9516562
Daniel <danielheinze@MacBook-Air-Daniel.local>
2024-07-20 20:12:26 +0200

    seeders for all Existing Models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Article;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Article::factory(100)->create();
+        $this->call([
+            UserSeeder::class,
+            ConversationSeeder::class,
+            MessageSeeder::class,
+            ArticleSeeder::class,
+        ]);
     }
 }
