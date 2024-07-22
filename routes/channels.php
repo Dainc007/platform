c58bc135ad124890659a6e7dc6296897987f5f39
Daniel <danielheinze@MacBook-Air-Daniel.local>
2024-07-22 15:50:15 +0200

    route update

--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('App.Models.Conversation.{id}', function ($conversation, $id) {
+return true;
+});
+
+Broadcast::channel('conversations.{id}', function () {
+    return true;
+});
