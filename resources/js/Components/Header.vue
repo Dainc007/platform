543d97bd1562d6a7026130b772dafa866081ae27
Daniel <danielheinze@MacBook-Air-Daniel.local>
2024-08-07 12:26:50 +0200

    Separate Header file

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <template>
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">{{ title }}</h2>
+    </template>
+</template>
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
