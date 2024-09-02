e34e701a044a33bbd15d16ad31abe653b2bddc8e
Daniel Heinze <danielheinze96@gmail.com>
2024-09-02 20:35:59 +0200

    Add DatePicker from Flowbite

--- resources/js/Pages/Vacation/Index.vue
+++ resources/js/Pages/Vacation/Index.vue
@@ -1,8 +1,17 @@
 <template>
     <AuthenticatedLayout>
-        <h1>Vacation</h1>
+        <div class="p-12">
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <div class="flex flex-col sm:flex-row justify-center items-center sm:items-start">
+                    <DatePicker class="w-full sm:w-auto"></DatePicker>
+                    <PrimaryButton class="mt-4 sm:mt-0 sm:ml-4 w-full sm:w-auto">Zgłoś Urlop</PrimaryButton>
+                </div>
+            </div>
+        </div>
     </AuthenticatedLayout>
 </template>
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import DatePicker from "@/Components/Date/DatePicker.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
 </script>
