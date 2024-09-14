fdd9348b16ece551694dd8d2b819b5cfd0318ac6
Wojtazz <marcin.witas72@gmail.com>
2024-09-14 21:44:37 +0200

    add roles crud

---
+++ app/Filament/Resources/RoleResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RoleResource\Pages;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+class RoleResource extends Resource
+{
+    protected static ?string $model = Role::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->string()
+                    ->required()
+                    ->maxLength(255)
+                    ->unique(ignoreRecord: true),
+                Forms\Components\CheckboxList::make('permissions')
+                    ->relationship(titleAttribute: 'name')
+                    ->exists(Permission::class, 'id'),
+                Forms\Components\Select::make('users')
+                    ->relationship(titleAttribute: 'email')
+                    ->multiple()
+                    ->searchable(['name', 'email'])
+                    ->exists(User::class, 'id'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRoles::route('/'),
+            'create' => Pages\CreateRole::route('/create'),
+            'edit' => Pages\EditRole::route('/{record}/edit'),
+        ];
+    }
+}
