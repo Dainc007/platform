4f7dbcc94aa3153b4ba1f328d50b47585d404062
Daniel Heinze <danielheinze96@gmail.com>
2024-10-03 19:19:41 +0200

    zjadłem } :D

--- app/Models/User.php
+++ app/Models/User.php
@@ -87,6 +87,7 @@ class User extends Authenticatable implements FilamentUser
     {
         return $this->hasMany(Meeting::class);
     }
+
       public function canAccessPanel(Panel $panel): bool
     {
         return $this->can('view panel');
