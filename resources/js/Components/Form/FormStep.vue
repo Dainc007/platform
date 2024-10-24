7a354c57e07427e30f1f3090fb2910bf86406d33
Daniel Heinze <danielheinze96@gmail.com>
2024-10-24 12:34:39 +0200

    MultiStepForm

---
+++ resources/js/Components/Form/FormStep.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+</script>
+<template>
+    <li class="flex items-center text-blue-600 dark:text-blue-500 space-x-2.5 rtl:space-x-reverse">
+<span class="flex items-center justify-center w-8 h-8 border border-blue-600 rounded-full shrink-0 dark:border-blue-500">
+1
+</span>
+        <span>
+<h3 class="font-medium leading-tight">{{ title }}</h3>
+<p class="text-sm">{{ description }}</p>
+</span>
+    </li>
+</template>
---
+++ resources/js/Components/Form/MultiStepForm.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import FormStep from "@/Components/Form/FormStep.vue";
+</script>
+
+<template>
+    <ol class="items-center w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0 rtl:space-x-reverse">
+        <FormStep title="User info" description="Step details here" />
+        <FormStep title="Company info" description="Step details here" />
+        <FormStep title="Payment info" description="Step details here" />
+    </ol>
+
+
+</template>
+
+<style scoped>
+
+</style>
