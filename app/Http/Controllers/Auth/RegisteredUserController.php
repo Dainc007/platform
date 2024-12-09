aba04bff06de966c60b3bb9d77479f9886fa9556
Daniel Heinze <danielheinze96@gmail.com>
2024-12-09 19:31:47 +0100

    wyłączenie mozliwości rejestracji

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -18,9 +18,10 @@ class RegisteredUserController extends Controller
     /**
      * Display the registration view.
      */
-    public function create(): Response
+    public function create(): RedirectResponse
     {
-        return Inertia::render('Auth/Register');
+        return redirect()->back();
+//        return Inertia::render('Auth/Register');
     }
 
     /**
@@ -30,22 +31,24 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        return redirect()->back();
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        event(new Registered($user));
-
-        Auth::login($user);
-
-        return redirect(route('dashboard', absolute: false));
+//        $request->validate([
+//            'name' => 'required|string|max:255',
+//            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+//            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+//        ]);
+//
+//        $user = User::create([
+//            'name' => $request->name,
+//            'email' => $request->email,
+//            'password' => Hash::make($request->password),
+//        ]);
+//
+//        event(new Registered($user));
+//
+//        Auth::login($user);
+//
+//        return redirect(route('dashboard', absolute: false));
     }
 }
