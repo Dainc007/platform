b417d756fc64ac1eb343c83f8e6f02d8b0c82f45
Daniel Heinze <danielheinze96@gmail.com>
2025-01-08 10:34:52 +0100

    Redirect to CRM

--- resources/js/Layouts/AuthenticatedTopNav.vue
+++ resources/js/Layouts/AuthenticatedTopNav.vue
@@ -27,6 +27,10 @@ const showingNavigationDropdown = ref(false);
 
                     <!-- Navigation Links -->
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        <a v-if="$page.props.auth.isAdmin"
+                            class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out'"
+                            href="https://crm.grupapwc.pl">CMS
+                        </a>
                         <template v-for="setting in usePage().props.settings">
                             <NavLink
                                 v-if="setting && setting.value === 1"
