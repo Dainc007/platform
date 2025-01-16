958a70cf124c0fdad9edd09be4513d2c72f25fc7
Daniel Heinze <danielheinze96@gmail.com>
2025-01-16 13:15:05 +0100

    Temporary set all policies to true

--- app/Policies/PricingPolicy.php
+++ app/Policies/PricingPolicy.php
@@ -2,11 +2,11 @@
 
 namespace App\Policies;
 
-use App\Models\Product;
+use App\Models\Pricing;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class ProductPolicy
+class PricingPolicy
 {
     /**
      * Determine whether the user can view any models.
@@ -19,7 +19,7 @@ class ProductPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Product $product): bool
+    public function view(User $user, Pricing $pricing): bool
     {
         return true;
     }
@@ -35,7 +35,7 @@ class ProductPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Product $product): bool
+    public function update(User $user, Pricing $pricing): bool
     {
         return true;
     }
@@ -43,7 +43,7 @@ class ProductPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Product $product): bool
+    public function delete(User $user, Pricing $pricing): bool
     {
         return true;
     }
@@ -51,16 +51,15 @@ class ProductPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Product $product): bool
+    public function restore(User $user, Pricing $pricing): bool
     {
         return true;
-
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Product $product): bool
+    public function forceDelete(User $user, Pricing $pricing): bool
     {
         return true;
     }
