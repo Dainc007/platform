7344299c3e928b2f5afd4b02bb8b21a5cd429400
Daniel Heinze <danielheinze96@gmail.com>
2025-01-17 13:54:10 +0100

    temporary hide nav items

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -17,6 +17,7 @@ class RoleResource extends Resource
     protected static ?string $model = Role::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static bool $shouldRegisterNavigation = false;
 
     public static function form(Form $form): Form
     {
